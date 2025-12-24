2a5af8e81ce4055d0741f6f3f65eafc15fa9dee7
elanuroguc-blip <elanuroguc@gmail.com>
2025-12-24 23:42:00 +0300

    Update ekipman.js

--- services/ekipman.js
+++ services/ekipman.js
@@ -1,8 +1,8 @@
 export const getEquipments = async (charId) => {
-    const data = {
+    const items = {
         1: { item: "Efsanevi Kalkan", bonusHp: 40, bonusAtk: 5 },
-        2: { id: 2, item: "Kadim Asa", bonusHp: 5, bonusAtk: 45 },
-        3: { id: 3, item: "Gölge Yayı", bonusHp: 15, bonusAtk: 25 }
+        2: { item: "Kadim Asa", bonusHp: 5, bonusAtk: 45 },
+        3: { item: "Gölge Yayı", bonusHp: 15, bonusAtk: 25 }
     };
-    return data[charId] || { item: "Eski Kılıç", bonusHp: 0, bonusAtk: 0 };
+    return items[charId] || { item: "Tahta Kılıç", bonusHp: 0, bonusAtk: 0 };
 };
