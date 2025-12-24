5eeb8e18f47f78febce8a309da21c1edcd031f49
elanuroguc-blip <elanuroguc@gmail.com>
2025-12-24 20:14:53 +0300

    Update ekipman.js

--- services/ekipman.js
+++ services/ekipman.js
@@ -1,9 +1,8 @@
 export const getEquipments = async (charId) => {
-    const equipmentMap = {
-        1: { item: "Çelik Zırh", bonusHp: 50, bonusAtk: 5 },
-        2: { item: "Kristal Asa", bonusHp: 10, bonusAtk: 40 },
-        3: { item: "Elven Yayı", bonusHp: 20, bonusAtk: 25 }
+    const data = {
+        1: { item: "Efsanevi Kalkan", bonusHp: 40, bonusAtk: 5 },
+        2: { id: 2, item: "Kadim Asa", bonusHp: 5, bonusAtk: 45 },
+        3: { id: 3, item: "Gölge Yayı", bonusHp: 15, bonusAtk: 25 }
     };
-    // Karakter ID'sine göre ilgili ekipmanı dönüyoruz
-    return equipmentMap[charId] || { item: "Paslı Kılıç", bonusHp: 0, bonusAtk: 0 };
+    return data[charId] || { item: "Eski Kılıç", bonusHp: 0, bonusAtk: 0 };
 };
