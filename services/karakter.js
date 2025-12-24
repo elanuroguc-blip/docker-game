b7b217df302483cfe451fe04c86e07db4ecc1559
elanuroguc-blip <elanuroguc@gmail.com>
2025-12-24 20:14:27 +0300

    Update karakter.js

--- services/karakter.js
+++ services/karakter.js
@@ -1,9 +1,8 @@
 export const getCharacters = async () => {
-    // Stateless veri: Sunucudan geliyormuş gibi simüle ediyoruz
-    const characters = [
-        { id: 1, name: "Savaşçı", baseHp: 150, baseAtk: 20, img: "🛡️" },
-        { id: 2, name: "Büyücü", baseHp: 80, baseAtk: 45, img: "🪄" },
-        { id: 3, name: "Okçu", baseHp: 110, baseAtk: 30, img: "🏹" }
+    // API simülasyonu (Stateless)
+    return [
+        { id: 1, name: "Savaşçı", baseHp: 150, baseAtk: 25, img: "🛡️" },
+        { id: 2, name: "Büyücü", baseHp: 80, baseAtk: 50, img: "🪄" },
+        { id: 3, name: "Okçu", baseHp: 110, baseAtk: 35, img: "🏹" }
     ];
-    return characters;
 };
